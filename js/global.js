81f514df132890bc709bab1c8184890cdb742e30
aaron-cheng <aaron@chengfolio.com>
2018-10-15 22:21:35 -0700

    Initial commit

---
+++ js/global.js
@@ -0,0 +1,63 @@
+/* ************************************************
+**************************************************
+
+Website Name: BorderX Lab
+Website URL: borderxlab.com
+Website Author: Aaron Cheng
+Author URL: chengfolio.com
+Copyright 2014. All Rights Reserved.
+
+************************************************ */
+
+/* ***********************************************
+*************************************************
+0. General -------------------- All Files
+*************************************************
+*********************************************** */
+
+$(document).ready(function(){	
+/*----------------------------------------------
+------------------------------------------------
+0. General
+------------------------------------------------
+----------------------------------------------*/	
+	$(".btn__menu_expand").on('tap', function(){
+
+
+		if ($(".nav__menu--mobile").css("display") == "none") {
+			$(".nav__menu--mobile").css("display","block");
+		} else {
+			$(".nav__menu--mobile").css("display","none");
+		}
+	});
+
+	$(".privacy_policy__btn--close").on('tap', function(){
+
+		$(".privacy_policy").css("display","none");
+
+	});
+
+	$(".privacy_policy__btn--open").on('tap', function(){
+		
+		$(".privacy_policy").css("display","block");
+
+	});
+
+	//set video heigth based on its width
+	var video_height = $(".video--full-width").width()*0.5625;
+
+	$(".video--full-width").css("height",video_height);
+
+	//Resize&Reposition video height when resive window		
+	$(window).resize(function(){
+		var video_height = $(".video--full-width").width()*0.5625;
+			
+		$(".video--full-width").css("height",video_height);
+	});
+/*----------------------------------------------
+------------------------------------------------
+1. Home Page js
+------------------------------------------------
+----------------------------------------------*/	
+
+});
